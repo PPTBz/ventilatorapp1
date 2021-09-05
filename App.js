2761aeb9e69868d8df83e9cb806b71d12e50a173
audreyevidente <85916669+audreyevidente@users.noreply.github.com>
2021-09-05 13:32:26 +0800

    added charts (but working on web browser only)

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import React from "react";
+import React, { useState } from "react";
 import {
   SafeAreaView,
   ScrollView,
@@ -9,9 +9,23 @@ import {
   Text,
   View,
 } from "react-native";
+import firebase from "firebase";
+
 import Dashboard from "./src/screens/Dashboard";
 import BreathingFlowSignal from "./src/screens/BreathingFlowSignal";
 import NasalPressure from "./src/screens/NasalPressure";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCgpYAc-1Z34f8EdmusW9YL9ERIMOBVZqQ",
+  authDomain: "ventilatorapp-c14c3.firebaseapp.com",
+  projectId: "ventilatorapp-c14c3",
+  storageBucket: "ventilatorapp-c14c3.appspot.com",
+  messagingSenderId: "102303671067",
+  appId: "1:102303671067:web:68278442946e827fa3a9be",
+  measurementId: "G-RM137MCC2B",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const Stack = createStackNavigator();
 
--- src/screens/BreathingFlowSignal.js
+++ src/screens/BreathingFlowSignal.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import Colors from "../theme/Colors";
 import ViewContainer, { styles } from "../components/ViewContainer";
+import { Line } from "react-chartjs-2";
 
 const BreathingFlowSignal = () => {
+  const [chartData, setChartData] = useState({});
+
+  const chart = () => {
+    setChartData({
+      labels: ["monday", "tuesday", "wednesday", "thursday", "friday"],
+      datasets: [
+        {
+          label: "Breathing Flow Signal (L•s^-1)",
+          data: [32, 45, 12, 76, 69],
+          backgroundColor: ["rgba(75, 192, 192, 0.6)"],
+          borderWidth: 4,
+        },
+      ],
+    });
+  };
+
+  useEffect(() => {
+    chart();
+  }, []);
+
   return (
     <ViewContainer backgroundColor={Colors.powderBlue}>
       <Text style={styles.text}>Breathing Flow Signal</Text>
+      <View style={{ height: "500px", width: "500px" }}>
+        <Line
+          data={chartData}
+          options={{
+            responsive: true,
+          }}
+        />
+      </View>
     </ViewContainer>
   );
 };
