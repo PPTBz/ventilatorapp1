16e7fe3ae5674b251142adea4fec7221ed145a07
audreyevidente <85916669+audreyevidente@users.noreply.github.com>
2021-09-08 17:24:54 +0800

    edited charts with react native

--- src/screens/BreathingFlowSignal.js
+++ src/screens/BreathingFlowSignal.js
@@ -2,37 +2,24 @@ import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import Colors from "../theme/Colors";
 import ViewContainer, { styles } from "../components/ViewContainer";
-import { Line } from "react-chartjs-2";
+import { LineChart } from "react-native-chart-kit";
 
 const BreathingFlowSignal = () => {
-  const [chartData, setChartData] = useState({});
-
-  const chart = () => {
-    setChartData({
-      labels: ["monday", "tuesday", "wednesday", "thursday", "friday"],
-      datasets: [
-        {
-          label: "Breathing Flow Signal (L•s^-1)",
-          data: [32, 45, 12, 76, 69],
-          backgroundColor: ["rgba(75, 192, 192, 0.6)"],
-          borderWidth: 4,
-        },
-      ],
-    });
-  };
-
-  useEffect(() => {
-    chart();
-  }, []);
-
   return (
     <ViewContainer backgroundColor={Colors.powderBlue}>
       <Text style={styles.text}>Breathing Flow Signal</Text>
       <View style={{ height: "500px", width: "500px" }}>
-        <Line
-          data={chartData}
-          options={{
-            responsive: true,
+        <LineChart
+          data={{
+            labels: ["monday", "tuesday", "wednesday", "thursday", "friday"],
+            datasets: [
+              {
+                label: "Breathing Flow Signal (L•s^-1)",
+                data: [32, 45, 12, 76, 69],
+                backgroundColor: ["rgba(75, 192, 192, 0.6)"],
+                borderWidth: 4,
+              },
+            ],
           }}
         />
       </View>
