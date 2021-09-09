5367df57b068eafc9df3ca75e376ef02563c27d2
audreyevidente <85916669+audreyevidente@users.noreply.github.com>
2021-09-09 17:15:19 +0800

    edited to react native

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import React, { useState } from "react";
+import React from "react";
 import {
   SafeAreaView,
   ScrollView,
--- src/screens/NasalPressure.js
+++ src/screens/NasalPressure.js
@@ -1,38 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { View, Text } from "react-native";
 import Colors from "../theme/Colors";
 import ViewContainer, { styles } from "../components/ViewContainer";
-import { Line } from "react-chartjs-2";
+import { LineChart } from "react-native-chart-kit";
 
 const NasalPressure = () => {
-  const [chartData, setChartData] = useState({});
-
-  const chart = () => {
-    setChartData({
-      labels: ["monday", "tuesday", "wednesday", "thursday", "friday"],
-      datasets: [
-        {
-          label: "Nasal Pressure (cmH2o)",
-          data: [32, 45, 12, 76, 69],
-          backgroundColor: ["rgba(75, 192, 192, 0.6)"],
-          borderWidth: 4,
-        },
-      ],
-    });
-  };
-
-  useEffect(() => {
-    chart();
-  }, []);
-
   return (
     <ViewContainer backgroundColor={Colors.powderBlue}>
       <Text style={styles.text}>Nasal Pressure</Text>
       <View style={{ height: "500px", width: "500px" }}>
-        <Line
-          data={chartData}
-          options={{
-            responsive: true,
+        <LineChart
+          data={{
+            labels: ["monday", "tuesday", "wednesday", "thursday", "friday"],
+            datasets: [
+              {
+                label: "Nasal Pressure (cmH2o)",
+                data: [32, 45, 12, 76, 69],
+                color: (opacity = 0) => `rgba(255, 255, 255, ${opacity})`,
+                strokeWidth: 4,
+              },
+            ],
           }}
         />
       </View>
