f9ba3170399b7ccef1d2c1b90f2ac72caa78de41
audreyevidente <85916669+audreyevidente@users.noreply.github.com>
2021-09-17 16:56:37 +0800

    working charts

--- src/screens/BreathingFlowSignal.js
+++ src/screens/BreathingFlowSignal.js
@@ -2,29 +2,59 @@ import React from "react";
 import { View, Text } from "react-native";
 import Colors from "../theme/Colors";
 import ViewContainer, { styles } from "../components/ViewContainer";
-import { LineChart } from "react-native-chart-kit";
+import { LineChart, YAxis, XAxis, Grid } from "react-native-svg-charts";
 
-const BreathingFlowSignal = () => {
-  return (
-    <ViewContainer backgroundColor={Colors.powderBlue}>
-      <Text style={styles.text}>Breathing Flow Signal</Text>
-      <View style={{ height: "500px", width: "500px" }}>
-        <LineChart
-          data={{
-            labels: ["monday", "tuesday", "wednesday", "thursday", "friday"],
-            datasets: [
-              {
-                label: "Breathing Flow Signal (L•s^-1)",
-                data: [32, 45, 12, 76, 69],
-                color: (opacity = 0) => `rgba(255, 255, 255, ${opacity})`,
-                strokeWidth: 4,
-              },
-            ],
+class BreathingFlowSignal extends React.PureComponent {
+  render() {
+    const data = [
+      50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80,
+    ];
+
+    const axesSvg = { fontSize: 10, fill: "grey" };
+    const verticalContentInset = { top: 10, bottom: 10 };
+    const xAxisHeight = 30;
+
+    return (
+      <>
+        <Text
+          style={{
+            padding: 20,
+            textAlign: "center",
+            fontWeight: "bold",
+            fontSize: 20,
+            color: "#009394",
           }}
-        />
-      </View>
-    </ViewContainer>
-  );
-};
+        >
+          Breathing Flow Signal
+        </Text>
+        <View style={{ height: 200, padding: 30, flexDirection: "row" }}>
+          <YAxis
+            data={data}
+            style={{ marginBottom: xAxisHeight }}
+            contentInset={verticalContentInset}
+            svg={axesSvg}
+          />
+          <View style={{ flex: 1, marginLeft: 10 }}>
+            <LineChart
+              style={{ flex: 1 }}
+              data={data}
+              contentInset={verticalContentInset}
+              svg={{ stroke: "#009394" }}
+            >
+              <Grid />
+            </LineChart>
+            <XAxis
+              style={{ marginHorizontal: -10, height: xAxisHeight }}
+              data={data}
+              formatLabel={(value, index) => index}
+              contentInset={{ left: 10, right: 10 }}
+              svg={axesSvg}
+            />
+          </View>
+        </View>
+      </>
+    );
+  }
+}
 
 export default BreathingFlowSignal;
--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -5,28 +5,65 @@ import ViewContainer, { styles } from "../components/ViewContainer";
 
 const Dashboard = ({ navigation }) => {
   return (
-    <ViewContainer>
-      <TouchableOpacity
-        style={{
-          padding: 16,
-          margin: 10,
-          backgroundColor: Colors.steelBlue,
-        }}
-        onPress={() => navigation.navigate("BreathingFlowSignal")}
-      >
-        <Text>Breathing Flow Signal</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={{
-          padding: 16,
-          margin: 10,
-          backgroundColor: Colors.steelBlue,
-        }}
-        onPress={() => navigation.navigate("NasalPressure")}
-      >
-        <Text>Nasal Pressure</Text>
-      </TouchableOpacity>
-    </ViewContainer>
+    <>
+      <ViewContainer>
+        <Text
+          style={{
+            padding: 20,
+            textAlign: "center",
+            fontWeight: "bold",
+            fontSize: 20,
+            color: "#009394",
+          }}
+        >
+          The Dashboard
+        </Text>
+        <TouchableOpacity
+          style={{
+            padding: 16,
+            margin: 10,
+            backgroundColor: "#009394",
+            width: 280,
+            height: 50,
+            borderRadius: 50,
+          }}
+          onPress={() => navigation.navigate("BreathingFlowSignal")}
+        >
+          <Text
+            style={{
+              color: "#ffffff",
+              textAlign: "center",
+              fontSize: "18",
+              fontWeight: "bold",
+            }}
+          >
+            Breathing Flow Signal
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            padding: 16,
+            margin: 10,
+            backgroundColor: "#009394",
+            width: 280,
+            height: 50,
+            borderRadius: 50,
+          }}
+          onPress={() => navigation.navigate("NasalPressure")}
+        >
+          <Text
+            style={{
+              color: "#ffffff",
+              textAlign: "center",
+              fontSize: "18",
+              fontWeight: "bold",
+            }}
+          >
+            Nasal Pressure
+          </Text>
+        </TouchableOpacity>
+      </ViewContainer>
+    </>
   );
 };
 
