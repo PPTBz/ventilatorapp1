9e1531e918668bf3482ca3afe8a9f1614854f1c6
audreyevidente <85916669+audreyevidente@users.noreply.github.com>
2021-12-21 17:42:52 +0800

    main

---
+++ ventilatorapp/Shared/ventilatorappApp.swift
@@ -0,0 +1,17 @@
+//
+//  ventilatorappApp.swift
+//  Shared
+//
+//  Created by Audrey Evidente on 12/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ventilatorappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
